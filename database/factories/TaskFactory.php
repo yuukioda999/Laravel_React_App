362c9e8cad76764611254c0ba67b4317b0c10499
yuukioda999 <yuuki46yuuki@icloud.com>
2022-05-24 15:54:17 +0900

    DB設定、マイグレーション、ファクトリー、テストデータ作成

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->realText(rand(15,40)),
+            'is_done' => $this->faker->boolean(10),
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+    }
+}
