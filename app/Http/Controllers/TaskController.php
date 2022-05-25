a9888fe905fe504d7888285093ea9c148ad5b174
yuukioda999 <yuuki46yuuki@icloud.com>
2022-05-25 15:15:11 +0900

    CRUD API作成、テストコード作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -20,14 +20,16 @@ class TaskController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreTaskRequest  $request
-     * @return \Illuminate\Http\Response
+     * @param App\Http\Requests\StoreTaskRequest $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(StoreTaskRequest $request)
     {
-        //
+        $task = Task::create($request->all());
+
+        return $task
+            ? response()->json($task, 201)
+            : response()->json([], 500);
     }
 
     /**
@@ -41,26 +43,28 @@ class TaskController extends Controller
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateTaskRequest  $request
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     /**
+     * @param App\Http\Requests\UpdateTaskRequest $request
+     * @param App\Models\Task;
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $task->title = $request->title;
+
+        return $task->update()
+            ? response()->json($task)
+            : response()->json([], 500);
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
      * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Task $task)
     {
-        //
+        return $task->delete()
+        ? response()->json($task)
+        : response()->json([], 500);
     }
 }
--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -14,11 +14,54 @@ class TaskTest extends TestCase
     /**
      * @test
      */
-    public function 一覧を取得()
+    public function 一覧を取得できる()
     {
         $tasks = Task::factory()->count(10)->create();
         $response = $this->getJson('api/tasks');
 
         $response->assertOk()->assertJsonCount($tasks->count());
+    }
+
+    /**
+     * @test
+     */
+    public function 登録することができる()
+    {
+        $data = [
+            'title' => 'テスト投稿',
+        ];
+
+        $response = $this->postJson('api/tasks',$data);
+        $response->assertCreated()->assertJsonFragment($data);
+    }
+
+    /**
+     * @test
+     */
+    public function 更新することができる()
+    {
+        $task = Task::factory()->create();
+        $task->title = '書き換え';
+
+        $response = $this->patchJson("api/tasks/{$task->id}",$task->toArray());
+        $response
+            ->assertOk()
+            ->assertJsonFragment($task->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function 削除することができる()
+    {
+        $tasks = Task::factory()->count(10)->create();
+
+        $response = $this->deleteJson("api/tasks/1");
+        $response->assertOk();
+        
+        $response = $this->getJson("api/tasks");
+
+        $response->assertJsonCount($tasks->count() - 1);
+
     }
 }
